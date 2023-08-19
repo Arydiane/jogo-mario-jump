e3bd9708095360b159dd595923cf134714959a58
Arydiane <arydianejardim@gmail.com>
2023-08-19 18:22:16 -0300

    adiciona funcionalidade pontuação

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@ const containerMsgGameOver = document.querySelector('.container-msg-game-over');
 const btnRestart = document.querySelector('.btn');
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const msgScore = document.querySelector('.msg-score');
 
 let startGame = true;
+let countScore = 0;
 
 const jump = () => {
     mario.classList.add('jump');
@@ -18,6 +20,8 @@ const loop = setInterval(() => {
     const marioPosition = Number(window.getComputedStyle(mario).bottom.replace('px', ''));
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        clearInterval(score)
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
@@ -32,16 +36,24 @@ const loop = setInterval(() => {
     }
 }, 10)
 
-btnRestart.addEventListener('click', () => {  
+const score = setInterval(() => {
+    if (startGame === false) {
+        countScore++;
+        msgScore.innerHTML = `Score: ${countScore}`
+    }
+}, 1000)
+
+btnRestart.addEventListener('click', () => {
     window.location.reload();
 });
 
 document.addEventListener('keydown', () => {
 
     if (startGame) {
-        msgInitial.style.display = 'none'; 
+        msgInitial.style.display = 'none';
+        msgScore.style.display = 'block';
         pipe.classList.add('move');
-        startGame = false; 
+        startGame = false;
     }
 
     jump();
