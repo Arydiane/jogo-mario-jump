952abf8e076e754152de389ea7538e9876368ee8
Arydiane <arydianejardim@gmail.com>
2023-08-19 14:42:35 -0300

    adiciona mensagem game over e botão reiniciar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const containerMsg = document.querySelector('.container-msg')
+const btnRestart = document.querySelector('.btn')
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
@@ -9,22 +11,26 @@ const jump = () => {
 }
 
 const loop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft; 
-    const marioPosition = Number(window.getComputedStyle(mario).bottom.replace('px', '')); 
-    
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = Number(window.getComputedStyle(mario).bottom.replace('px', ''));
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
         mario.src = "./images/game-over.png";
-        mario.style.width = '75px'; 
-        mario.style.marginLeft = '50px'; 
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
 
+        containerMsg.style.display = 'block';
         clearInterval(loop);
     }
-
 }, 10)
+
+btnRestart.addEventListener('click', () => {
+    window.location.reload()
+})
 
 document.addEventListener('keydown', jump);
